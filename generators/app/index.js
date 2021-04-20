25ab0742914c94da66c23a0653cb06c70b48d51b
Angel Eduardo <angel.gonzalez@smfconsulting.com>
2021-04-20 17:26:26 -0300

    Add main generator for selecting either component or action generation

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,18 +1,33 @@
 "use strict";
 const Generator = require("yeoman-generator");
-const chalk = require("chalk");
-const yosay = require("yosay");
 
+var yeoman = require("yeoman-environment");
+var env = yeoman.createEnv();
 module.exports = class extends Generator {
-  initializing() {
-    this.log(
-      yosay(
-        `The generator does nothing by itself. Please, run ${chalk.bold(
-          "yo pos2:component"
-        )}, ${chalk.bold("yo pos2:useraction")} or ${chalk.bold(
-          "yo pos2:stateaction"
-        )}`
-      )
-    );
+  prompting() {
+    const prompts = [
+      {
+        type: "list",
+        name: "generator",
+        message: "Choose a generation type: ",
+        choices: ["Component", "Action / User Action"]
+      }
+    ];
+
+    return this.prompt(prompts).then(props => {
+      this.props = props;
+    });
+  }
+
+  finishing() {
+    let namespace = "";
+    if (this.props.generator === "Component") {
+      namespace = ":component";
+    } else if (this.props.generator === "Action / User Action") {
+      namespace = ":action";
+    }
+
+    const generator = env.create(`pos-2${namespace}`);
+    generator.run();
   }
 };
